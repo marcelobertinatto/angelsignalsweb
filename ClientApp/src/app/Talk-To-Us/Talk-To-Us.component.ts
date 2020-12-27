aa65374837cee49a2c52e0cb629ac9c5b3a24d64
Marcelo Bertinatto <marcelobertinatto@hotmail.com>
2020-12-27 15:44:18 +0000

    missing files

---
+++ ClientApp/src/app/Talk-To-Us/Talk-To-Us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Talk-To-Us',
+  templateUrl: './Talk-To-Us.component.html',
+  styleUrls: ['./Talk-To-Us.component.css']
+})
+export class TalkToUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
